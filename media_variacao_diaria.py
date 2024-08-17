b654eb017c848e67683ca22faea2741850bb6e52
George Telles <george_1@hotmail.com.br>
2024-08-17 18:43:21 -0300

    Update media_variacao_diaria.py

--- media_variacao_diaria.py
+++ media_variacao_diaria.py
@@ -19,11 +19,13 @@ else:
 data = pd.DataFrame(mt5.copy_rates_from_pos(symbol, mt5.TIMEFRAME_D1, 0, 2000))
 data["time"] = pd.to_datetime(data["time"], unit = "s")
 data["variacao"] = data["close"] - data["open"]
-print(data)
+
 
 dados_analisados = pd.Series(data["variacao"])
 figure(figsize = (10, 6), dpi = 80)
 dados_analisados.plot.hist(grid=True, bins=100, rwidth=0.9, color = "#607c8e")
+
+print(dados_analisados.describe())
 
 plt.title("Variação de pontos desde a abertura")
 plt.xlabel("Quantidade de pregões")
